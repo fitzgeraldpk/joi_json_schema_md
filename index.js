763c95a2d83a57cb7e28ba834e78300f6d7e3e9f
Patrick Fitzgerald <fitzgeraldpk@gmail.com>
2019-04-15 14:55:01 +0100

    check for relative or abs path

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const path=require('path'),
       fs=require('fs',)
       convert = require('joi-to-json-schema'),
       convertArgs = require('commander'),
-      exec = require('child_process').exec;
+      exec = require('child_process').exec
+      normalizedPath='';
 
      convertArgs.version('0.1.0')
      .option('-d,--dir <dir>')
      .option('-o,--output <output>')
      .parse(process.argv);
-
-    var normalizedPath = path.join(__dirname, convertArgs.dir);
-
+     if(path.isAbsolute(convertArgs.dir)) {
+        normalizedPath=convertArgs.dir;
+    }else{
+        normalizedPath = path.join(__dirname, convertArgs.dir);
+    } 
     fs.readdir(normalizedPath,function(err,files) {       
         if(err){
             console.log(err);
